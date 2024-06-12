fe659100a0e48c61086afd84f997cbf264b3ac73
Pietro Fusco <pietrofusco84@gmail.com>
2024-06-12 18:16:32 +0200

    revert

--- src/main/java/com/example/CommunistDate/config/CorsConfig.java
+++ src/main/java/com/example/CommunistDate/config/CorsConfig.java
@@ -1,19 +1,18 @@
-// package com.example.CommunistDate.config;
+package com.example.CommunistDate.config;
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-// import jakarta.annotation.Nonnull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import jakarta.annotation.Nonnull;
 
-// @Configuration
-// public class CorsConfig implements WebMvcConfigurer {
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
 
-//     @Override
-    
-//     public void addCorsMappings(@Nonnull CorsRegistry registry) {
-//         registry.addMapping("/**")
-//                 .allowedOrigins("http://127.0.0.1:5173/") 
-//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                 .allowedHeaders("*");
-//     }
-// }
+    @Override
+    public void addCorsMappings(@Nonnull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5173/") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
