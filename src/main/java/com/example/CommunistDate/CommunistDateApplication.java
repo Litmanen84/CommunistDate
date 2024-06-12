ba2ea1b4b439cf17db83463c219eb45cad293415
Pietro Fusco <pietrofusco84@gmail.com>
2024-06-12 18:16:32 +0200

    revert

--- src/main/java/com/example/CommunistDate/CommunistDateApplication.java
+++ src/main/java/com/example/CommunistDate/CommunistDateApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-// @EnableWebMvc 
+@EnableWebMvc 
 @ComponentScan(basePackages = {"com.example.CommunistDate"})
 public class CommunistDateApplication extends SpringBootServletInitializer {
 
