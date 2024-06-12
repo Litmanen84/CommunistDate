29f6cc744dc14940bc4f014274397632d7cad49b
Pietro Fusco <pietrofusco84@gmail.com>
2024-06-12 15:40:06 +0200

    PORT

--- src/main/java/com/example/CommunistDate/CommunistDateApplication.java
+++ src/main/java/com/example/CommunistDate/CommunistDateApplication.java
@@ -2,12 +2,26 @@ package com.example.CommunistDate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-public class CommunistDateApplication {
+@ComponentScan(basePackages = {"com.example.CommunistDate"})
+public class CommunistDateApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommunistDateApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Specifica la porta Heroku dalla variabile d'ambiente $PORT
+        String port = System.getenv("PORT");
+        if (port == null || port.isEmpty()) {
+            port = "8080"; // Porta predefinita nel caso la variabile d'ambiente non sia disponibile
+        }
+        
+        // Configura il server web Spring Boot per ascoltare sulla porta fornita
+        System.setProperty("server.port", port);
 
+        // Avvia l'applicazione Spring Boot
+        SpringApplication.run(CommunistDateApplication.class, args);
+    }
 }
+
