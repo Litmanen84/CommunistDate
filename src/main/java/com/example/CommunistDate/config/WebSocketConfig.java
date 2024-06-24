cd5e8fff414ff780552c90b27cbad2c70910f4e7
Pietro Fusco <pietrofusco84@gmail.com>
2024-06-24 16:41:51 +0200

    Chat history

--- src/main/java/com/example/CommunistDate/config/WebSocketConfig.java
+++ src/main/java/com/example/CommunistDate/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/queue");
-        config.setUserDestinationPrefix("/chat");
+        config.setUserDestinationPrefix("/user");
         // Rimuovi o non definire il prefisso delle destinazioni delle applicazioni
     }
 
