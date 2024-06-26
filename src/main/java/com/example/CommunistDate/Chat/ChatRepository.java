f811b2a9b7c5334f8df82202d1e6768076206c69
Pietro Fusco <pietrofusco84@gmail.com>
2024-06-26 15:22:48 +0200

    Modified chatHistory method

--- src/main/java/com/example/CommunistDate/Chat/ChatRepository.java
+++ src/main/java/com/example/CommunistDate/Chat/ChatRepository.java
@@ -2,8 +2,13 @@ package com.example.CommunistDate.Chat;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
+	
 	List<Chat> findBySenderIdOrReceiverId(Long senderId, Long receiverId);
-	List<Chat> findBySenderIdAndReceiverIdOrReceiverIdAltAndSenderIdAlt(Long senderId, Long receiverId, Long receiverIdAlt, Long senderIdAlt);
+
+	@Query("SELECT c FROM Chat c WHERE (c.sender.id = :userId1 AND c.receiver.id = :userId2) OR (c.sender.id = :userId2 AND c.receiver.id = :userId1)")
+    List<Chat> findChatHistory(@Param("userId1") Long userId1, @Param("userId2") Long userId2);
 }
